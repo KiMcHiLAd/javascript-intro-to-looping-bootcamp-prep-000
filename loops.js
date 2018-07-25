9510f26decf344e5b0b946a449bf4550e0d2c14b
Hania Bigo <hrbigos@umich.edu>
2018-07-25 20:48:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,9 @@ function whileLoop(n) {
 
 
 function doWhileLoop(array) {
-  do {
+function maybeTrue() {
+  return Math.random() >= 0.5
+}  do {
     array.pop();
    } while (array.length > -1 && maybeTrue());
     return array;
